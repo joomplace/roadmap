d976ccfc942dfac9c21ef9286fab2655134057a1
Alexandr Kosarev <alex7r@ya.ru>
2018-09-09 23:18:52 +0300

    Commit a simple extension

---
+++ admin/roadmap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright (c) 2018. JoomPlace, all rights reserved
+ */
+
+namespace Joomplace\Component\Roadmap\Admin;
+
+echo "<pre>";
+print_r('Say hello to the world of the simpliest components!');
+echo "</pre>";
