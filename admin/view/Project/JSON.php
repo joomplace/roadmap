0981e6523baa1bb430f43c909d04f807fb5e3fbe
Alexandr Kosarev <alex7r@ya.ru>
2018-09-10 23:20:26 +0300

    make a RESTApi for "Project"

---
+++ admin/view/Project/JSON.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 2018. JoomPlace, all rights reserved
+ */
+
+namespace Joomplace\Component\Roadmap\Admin\view\Project;
+
+
+use Joomplace\X\Renderer\Api;
+use Joomplace\X\View;
+
+class JSON extends View
+{
+    use Api;
+}
